7bc0d77ad4129d847033583d3a2b9a060c527486
Guilherme Quental <guilherme@quent.al>
2017-02-27 10:41:13 -0300

    Add winner logic to the board

--- src/components/board.js
+++ src/components/board.js
@@ -5,7 +5,8 @@ class Board extends Component {
   constructor() {
     super()
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
   
@@ -15,12 +16,26 @@ class Board extends Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = "X"
-    this.setState({squares})
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? "X" : "O"
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = `Winner ${winner}`
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -42,6 +57,26 @@ class Board extends Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board
