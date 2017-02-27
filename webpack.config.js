7a2edf35a4e55d9c40a3453032665d927a24a133
Guilherme Quental <guilherme@quent.al>
2017-02-26 22:46:48 -0300

    Add webpack config for the proejct

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    context: path.resolve(__dirname, "./src"),
+    entry: {
+        app: "./app.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist/assets"),
+        filename: "[name].bundle.js",
+        publicPath: "/assets"
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "./src")
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: "babel-loader",
+                    options: { presets: ["es2015", "react"] }
+                }]
+            },
+            {
+                test: /\.css?$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+
+}
